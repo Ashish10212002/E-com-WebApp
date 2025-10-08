3b4d4cebb877587b9e01edb6aa9dbc719bb19a76
Ashish patel <patelap0108@gmail.com>
2025-10-08 18:05:09 +0530

    Initial commit

---
+++ src/main/java/com/ashish/web_proj/model/Product.java
@@ -0,0 +1,56 @@
+package com.ashish.web_proj.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+	
+
+	 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String description;
+	private String brand;
+	private BigDecimal price;
+	private String category;
+//	@Column(name = "release_date")
+	private Date releaseDate;
+	
+//	@Column(name = "available")
+	private Boolean productAvailable;
+	
+//	@Column(name = "quantity")
+	private int stockQuantity;
+	 
+	private String imageName;
+	private String imageType;
+	@Lob
+//	@Column(name = "image_data", columnDefinition = "MEDIUMBLOB") 
+	private byte[] imageDate;
+
+	
+}
